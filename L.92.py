6f7db8a55cf426ffb1fb65eab8209871aefcfd80
dkhongell <116841735+dkhongell@users.noreply.github.com>
2022-10-27 16:28:34 -0400

    Add files via upload

---
+++ L.92.py
@@ -0,0 +1,14 @@
+#Brenna and Kalena
+string='SPAM!HelloSPAM! worldSPAM!!'
+def remove_substring(string, remove):
+    output=[]
+    index=0
+    while index < len(string):
+        if string[index:index+ len(remove)]==remove:
+            index += len(remove)
+        else:
+            output.append(string[index])
+            index+=1
+    print("".join(output))
+
+    
